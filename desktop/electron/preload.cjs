1fe93cdceea734abdad8e97a272723c50f59cab1
ashraftefa97-beep <ashraftefa97@gmail.com>
2026-09-21 22:38:29 +0300

    Expose secure DAI Professional session bridge

--- desktop/electron/preload.cjs
+++ desktop/electron/preload.cjs
@@ -8,4 +8,7 @@ contextBridge.exposeInMainWorld('daiDesktop', {
   pickAndOpenFile: () => ipcRenderer.invoke('dai:pick-file'),
   getStartup: () => ipcRenderer.invoke('dai:get-startup'),
   setStartup: (enabled) => ipcRenderer.invoke('dai:set-startup', Boolean(enabled)),
+  setSession: (accessToken) => ipcRenderer.invoke('dai:set-session', String(accessToken || '')),
+  clearSession: () => ipcRenderer.invoke('dai:clear-session'),
+  runningApps: () => ipcRenderer.invoke('dai:running-apps'),
 });
