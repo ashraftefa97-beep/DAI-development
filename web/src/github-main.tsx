93f0796e818a392476b17ab4f0acf5a01cf85d85
ashraftefa97-beep <ashraftefa97@gmail.com>
2026-09-21 13:06:29 +0300

    feat: add standalone GitHub Pages DAI mode

---
+++ web/src/github-main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GithubApp from './GithubApp';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode><GithubApp/></React.StrictMode>
+);
