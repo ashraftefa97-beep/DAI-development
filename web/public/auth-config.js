762cdda18e8167da532bd9b101e461acb86166ff
ashraftefa97-beep <ashraftefa97@gmail.com>
2026-09-21 13:37:00 +0300

    config: connect DAI auth to Supabase

--- web/public/auth-config.js
+++ web/public/auth-config.js
@@ -1,1 +1,4 @@
-window.DAI_AUTH_CONFIG = { url: '', publishableKey: '' };
+window.DAI_AUTH_CONFIG = {
+  url: 'https://buenonmbyudjhpedmoqk.supabase.co',
+  publishableKey: 'sb_publishable_uo9ZnHKtD-aDpE_zSJTaJg_inIzQ_Gt'
+};
