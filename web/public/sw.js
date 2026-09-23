aeaf0913a2117c6bfa461a88dd069f0edcd3d9cb
ashraftefa97-beep <ashraftefa97@gmail.com>
2026-09-24 01:29:51 +0300

    Bust DAI cache for settings redesign

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dai-web-v5-20260924';
+const CACHE_NAME = 'dai-web-v6-20260924';
 const CORE = ['./', './dai-logo.svg', './auth-config.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
