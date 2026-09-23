a6446ee9a6936f7a125423cb25521c8d0d8c8bc3
ashraftefa97-beep <ashraftefa97@gmail.com>
2026-09-24 02:08:34 +0300

    Bust DAI cache for avatar system v1.9

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dai-web-v8-20260924';
+const CACHE_NAME = 'dai-web-v9-20260924';
 const CORE = ['./', './dai-logo.svg', './auth-config.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
