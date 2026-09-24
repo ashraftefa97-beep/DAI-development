194e74e085e1d15516d374eccf67da3585453c6d
ashraftefa97-beep <ashraftefa97@gmail.com>
2026-09-24 18:37:31 +0300

    remove Nika visual DNA

--- web/src/avatarVisualDNA.mjs
+++ web/src/avatarVisualDNA.mjs
@@ -22,8 +22,7 @@ export const AVATAR_VISUAL_DNA=Object.freeze({
   galaxy:{eye:'star',mouth:'cosmic',hand:'orbit',handMark:'star',signature:'planetRing'},
   desert:{eye:'dune',mouth:'duneWave',hand:'sand',handMark:'sun',signature:'duneSun'},
   lavender:{eye:'butterfly',mouth:'lavenderArc',hand:'petal',handMark:'flower',signature:'butterfly'},
-  matrix:{eye:'square',mouth:'code',hand:'digital',handMark:'code',signature:'codeRain'},
-  bonney_nika:{eye:'nikaSpark',mouth:'nikaGrin',hand:'cloud',handMark:'crescent',signature:'nikaCloudCrown'}
+  matrix:{eye:'square',mouth:'code',hand:'digital',handMark:'code',signature:'codeRain'}
 });
 
 export function getAvatarVisualDNA(id='classic'){
