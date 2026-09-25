82d912e4d3d005f461ce0be5cc7283b818844a21
ashraftefa97-beep <ashraftefa97@gmail.com>
2026-09-25 04:48:20 +0300

    chore: refresh cache for articulated lip sync

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dai-web-v11-20260925-presence';
+const CACHE_NAME = 'dai-web-v12-20260925-articulation';
 const CORE = ['./', './dai-logo.svg', './auth-config.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
