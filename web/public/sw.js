01725348c373aa14e48e697ec25d3f43da49837b
ashraftefa97-beep <ashraftefa97@gmail.com>
2026-09-25 05:36:32 +0300

    chore: refresh cache for dark glass intro

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dai-web-v16-20260925-classic-intro';
+const CACHE_NAME = 'dai-web-v17-20260925-dark-header';
 const CORE = ['./', './dai-logo.svg', './auth-config.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
